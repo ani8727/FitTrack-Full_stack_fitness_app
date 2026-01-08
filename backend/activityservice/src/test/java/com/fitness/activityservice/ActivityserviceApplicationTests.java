8eede5adeb88ddfbfb5a7d42c028e9c6ad99e2e9
ani8727 <aniketgupta.8727@gmail.com>
2026-01-08 19:52:10 +0530

    Centralize config

---
+++ backend/activityservice/src/test/java/com/fitness/activityservice/ActivityserviceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.fitness.activityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ActivityserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
