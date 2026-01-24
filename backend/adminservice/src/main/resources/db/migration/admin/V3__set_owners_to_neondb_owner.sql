6aea980cc9f901a3cb3dd414f6409134f99cb898
ani8727 <aniketgupta.8727@gmail.com>
2026-01-25 00:07:21 +0530

    admin update

--- backend/adminservice/src/main/resources/db/migration/admin/V3__set_owners_to_neondb_owner.sql
+++
@@ -1,8 +0,0 @@
--- Set table owners to neondb_owner in admin_db (public schema)
--- Note: Requires privileges to change ownership (must be current owner or superuser)
-
-ALTER TABLE IF EXISTS public.users OWNER TO neondb_owner;
-ALTER TABLE IF EXISTS public.admin_audit_logs OWNER TO neondb_owner;
-
--- Change owner of identity sequence if present
-ALTER SEQUENCE IF EXISTS public.admin_audit_logs_id_seq OWNER TO neondb_owner;
