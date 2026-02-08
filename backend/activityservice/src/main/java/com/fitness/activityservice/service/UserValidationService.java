5c5eb4f2a6b3606f04d2d1553535206796cda0e6
ani8727 <aniketgupta.8727@gmail.com>
2026-02-08 22:55:50 +0530

    fix

--- backend/activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
+++ backend/activityservice/src/main/java/com/fitness/activityservice/service/UserValidationService.java
@@ -1,11 +1,11 @@
 package com.fitness.activityservice.service;
 
-import org.springframework.stereotype.Service;
+import java.util.Map;
+
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
-
-import java.util.Map;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
